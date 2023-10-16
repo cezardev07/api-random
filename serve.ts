f6056ec3a80ca735f45d3faf0b344cc03889c394
cezardev07 <cezardev07@gmail.com>
2023-10-16 18:00:16 -0300

    feat: website api

--- serve.ts
+++ serve.ts
@@ -1,9 +1,15 @@
 import "dotenv/config";
 import app from "./app/app";
+import express from "express";
 
 const PORT = process.env.PORT || 3333;
 
 const serve = app.serve();
+
+serve.use("/public", express.static("public"))
+serve.get("/home", (req, res) => {
+  return res.sendFile(__dirname + "/public/index.html")
+});
 
 serve.listen(PORT, callback);
 
